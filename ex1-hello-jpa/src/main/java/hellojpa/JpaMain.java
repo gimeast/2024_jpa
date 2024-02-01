f0a2b8a9e5e2f8b925d255ad191147184cdc9332
gimeast <dudu.gimeast@gmail.com>
2024-02-02 00:36:14 +0900

    Hello JPA - 애플리케이션 개발 - JPA를 이용한 CRUD

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -10,7 +12,48 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("TesterB");
+
+//            em.persist(member); //등록
+
+
+//            Member member = em.find(Member.class, 1L);
+//            System.out.println("member.getId: " + member.getId());
+//            System.out.println("member.getId: " + member.getName());
+
+//            em.remove(member); //삭제
+
+
+//            Member member = em.find(Member.class, 2L);
+//            member.setName("HelloJPA"); //수정
+
+            int pageNumber = 1;
+            int pageSize = 10;
+
+            List<Member> memberList = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult((pageNumber-1) * pageSize)
+                    .setMaxResults(pageSize)
+                    .getResultList();
+
+            System.out.println("=========================================");
+
+            for (Member member : memberList) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
