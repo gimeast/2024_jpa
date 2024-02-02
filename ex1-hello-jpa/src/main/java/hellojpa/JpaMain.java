4085193a1faf9950f24caaee55a9e6f38758dd62
gimeast <dudu.gimeast@gmail.com>
2024-02-02 23:26:44 +0900

    영속성 컨텍스트 1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -33,21 +33,42 @@ public class JpaMain {
 //            Member member = em.find(Member.class, 2L);
 //            member.setName("HelloJPA"); //수정
 
-            int pageNumber = 1;
-            int pageSize = 10;
+//            int pageNumber = 1;
+//            int pageSize = 10;
 
-            List<Member> memberList = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult((pageNumber-1) * pageSize)
-                    .setMaxResults(pageSize)
-                    .getResultList();
+//            List<Member> memberList = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult((pageNumber-1) * pageSize)
+//                    .setMaxResults(pageSize)
+//                    .getResultList();
 
-            System.out.println("=========================================");
+//            System.out.println("=========================================");
 
-            for (Member member : memberList) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            for (Member member : memberList) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+
+
+            //비영속 상태
+            Member member = new Member();
+            member.setId(103L);
+            member.setName("HelloJPA");
+            //여기까지 비영속 상태
+
+            System.out.println("BEFORE");
+            //영속 상태
+            em.persist(member);
+            System.out.println("AFTER");
+
+            //영속성 분리, 준영속 상태
+//            em.detach(member);
+
+            Member findMember = em.find(Member.class, 101L);
+            //객체를 삭제한 상태
+            em.remove(findMember);
 
             tx.commit();
+
+            System.out.println("FINISH");
         } catch (Exception e) {
             tx.rollback();
         } finally {
