dc81f6d9230f9ced50c0d00c0de2d5f5903018b2
gimeast <dudu.gimeast@gmail.com>
2024-02-03 00:04:34 +0900

    영속성 컨텍스트 2

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -49,22 +49,43 @@ public class JpaMain {
 
 
             //비영속 상태
-            Member member = new Member();
-            member.setId(103L);
-            member.setName("HelloJPA");
+//            Member member = new Member();
+//            member.setId(104L);
+//            member.setName("HelloJPA");
             //여기까지 비영속 상태
 
-            System.out.println("BEFORE");
+//            System.out.println("BEFORE");
             //영속 상태
-            em.persist(member);
-            System.out.println("AFTER");
+//            em.persist(member);
+//            System.out.println("AFTER");
 
             //영속성 분리, 준영속 상태
 //            em.detach(member);
 
-            Member findMember = em.find(Member.class, 101L);
+            //객체 조회
+//            Member findMember1 = em.find(Member.class, 104L);
+//            Member findMember2 = em.find(Member.class, 104L);
             //객체를 삭제한 상태
-            em.remove(findMember);
+//            em.remove(findMember);
+
+//            System.out.println("findMember1.id = " + findMember1.getId());
+//            System.out.println("findMember1.name = " + findMember1.getName());
+//            System.out.println("==============================================");
+//            System.out.println("findMember2.id = " + findMember2.getId());
+//            System.out.println("findMember2.name = " + findMember2.getName());
+
+//            System.out.println("findMember1 == findMember2 : " + (findMember1 == findMember2)); //영속성 컨텍스트에 저장되어있는 있는것을 바라보므로 동일한 객체이다
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+
+            Member findMember = em.find(Member.class, 150L);
+            findMember.setName("ZZZZZZZ"); //update
+
+            System.out.println("findMember.name = " + findMember.getName());
+            System.out.println("==========================");
 
             tx.commit();
 
