49c98e34fd3d3e4de770c0f2ab5824b2969e1663
gimeast <dudu.gimeast@gmail.com>
2024-02-15 23:48:29 +0900

    실전 예제 6 - 값 타입 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -12,11 +13,33 @@ public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
-    private DeliveryStatus status;
+    @Embedded
+    private Address address;
 
     @OneToOne(fetch = FetchType.LAZY, mappedBy = "delivery")
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
