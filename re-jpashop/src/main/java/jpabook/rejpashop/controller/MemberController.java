97c4791536390a02781e6ba780cbfd53823425f7
gimeast <dudu.gimeast@gmail.com>
2024-03-05 01:02:39 +0900

    회원 등록

---
+++ re-jpashop/src/main/java/jpabook/rejpashop/controller/MemberController.java
@@ -0,0 +1,48 @@
+package jpabook.rejpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.rejpashop.domain.Address;
+import jpabook.rejpashop.domain.Member;
+import jpabook.rejpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        log.info("회원가입 진입");
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid @ModelAttribute MemberForm memberForm, BindingResult bindingResult) {
+        log.info("회원가입 등록 진입");
+
+        if (bindingResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipcode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
