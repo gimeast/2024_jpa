5ee4b410957625717644eecde3294f9eeb958635
gimeast <dudu.gimeast@gmail.com>
2024-03-07 13:01:23 +0900

    상품 목록

--- re-jpashop/src/main/java/jpabook/rejpashop/controller/ItemController.java
+++ re-jpashop/src/main/java/jpabook/rejpashop/controller/ItemController.java
@@ -1,12 +1,16 @@
 package jpabook.rejpashop.controller;
 
 import jpabook.rejpashop.domain.item.Book;
+import jpabook.rejpashop.domain.item.Item;
 import jpabook.rejpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +27,14 @@ public class ItemController {
     @PostMapping("/items/new")
     public String create(BookForm bookForm) {
         itemService.saveItem(Book.createBook(bookForm));
-        return "redirect:/";
+        return "redirect:/items";
+    }
+    
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/list";
     }
 
 }
