2427b29e238883ec43c1b2498c623aa172f48935
gimeast <dudu.gimeast@gmail.com>
2024-03-14 23:19:18 +0900

    주문 조회 V1: 엔티티 직접 노출

---
+++ re-jpashop/src/main/java/jpabook/rejpashop/api/OrderApiController.java
@@ -0,0 +1,34 @@
+package jpabook.rejpashop.api;
+
+import jpabook.rejpashop.domain.Order;
+import jpabook.rejpashop.domain.OrderItem;
+import jpabook.rejpashop.repository.OrderRepository;
+import jpabook.rejpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(orderItem -> orderItem.getItem().getName());
+        }
+
+        return all;
+    }
+
+}
