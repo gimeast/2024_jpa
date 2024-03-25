231fcb7545f77ba88b3977f6f8da3b212c03f6d4
gimeast <dudu.gimeast@gmail.com>
2024-03-25 23:41:50 +0900

    새로운 엔티티를 구별하는 방법

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -20,7 +20,7 @@ public class MemberController {
 
     private final MemberRepository memberRepository;
 
-    @PostConstruct
+//    @PostConstruct
     public void init() {
         for (int i = 0; i < 100; i++) {
             memberRepository.save(new Member("user"+i, i, null));
